e148bc02db44b4eaab5f5e89e0d9060d95e31a16
Stephen Kyle Laroco <162328445+SkvLaroco@users.noreply.github.com>
2026-03-23 13:17:28 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -76,5 +76,5 @@ def forecast():
 
 # ✅ IMPORTANT FOR RENDER
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
